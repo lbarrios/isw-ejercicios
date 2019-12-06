beba2ddccd0d50ddfd9e1a995b81844603079e8a
Ezequiel Barrios <ebarrios@medallia.com>
2019-12-06 03:28:23 -0300

    login

--- 25-TusLibros/4/client/src/components/LogInInputView.js
+++ 25-TusLibros/4/client/src/components/LogInInputView.js
@@ -29,7 +29,8 @@ class LogInInputComponent extends React.Component {
         return response.json()
       })
       .then(function (json) {
-        router.navigate("/catalog", { clientId, password ,cartId: json })
+        console.log('Ok, going to catalog');
+        router.navigate("/catalog", { clientId, password ,cartId: json });
       })
       .catch(function (error) {
         console.log('Looks like there was a problem: \n', error);
@@ -48,25 +49,64 @@ class LogInInputComponent extends React.Component {
     } = this.props
 
     return (
-      <div>
-        <Typography component="h1" gutterBottom>
+      <Container component="main" maxWidth="xs">
+      <div className="login-page" style={loginPage}>
+        <Typography component="h1" variant="h5">
           Ingrese usuario y contraseña:
-          </Typography>
-        <TextField className={classes.textField} value={clientId} label="Username" onChange={(ev)=>this.handleChange(ev,"clientId")}/>
-        <TextField className={classes.textField} type="password" value={password} label="Password" onChange={(ev)=>this.handleChange(ev,"password")}/>
-        
-
-        <Button
-          color="inherit"
-          onClick={(ev)=>this.handleSend(ev)}>
-          Create cart
-      </Button>
+        </Typography>
+        <form action="#catalog" className="form" style={form} onSubmit={(ev)=>this.handleSend(ev)}>
+          <TextField 
+            variant="outlined" 
+            className={classes.textField}
+            margin="normal"
+            required
+            fullWidth
+            value={clientId}
+            id="clientId"
+            label="Usuario"
+            //autoComplete="clientId"
+            onChange={(ev)=>this.handleChange(ev,"clientId")}
+            autoFocus
+          />
+          <TextField 
+            variant="outlined" 
+            className={classes.textField}
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            type="password"
+            value={password}
+            label="Password"
+            //autoComplete="password"
+            onChange={(ev)=>this.handleChange(ev,"password")}/>
+          <Button 
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={(ev)=>this.handleSend(ev)}>Crear Carrito
+          </Button>
+        </form>
       </div>
+      </Container>
     )
   }
 }
 
+const loginPage = {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+};
+
+const form = {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+};
+
 // Add style
 const LogInInputView = withStyles(styles, {
   withTheme: true
-})(LogInInputComponent)
+})(LogInInputComponent);
